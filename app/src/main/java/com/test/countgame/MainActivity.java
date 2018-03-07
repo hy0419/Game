7992302325c2f3c1047c5441846dff1375ca86be
yuto <yuto@hiragayuutonoMacBook-Pro.local>
2018-03-08 00:00:08 +0900

    second

--- app/src/main/java/com/test/countgame/MainActivity.java
+++ app/src/main/java/com/test/countgame/MainActivity.java
@@ -1,8 +1,14 @@
 package com.test.countgame;
 
 import android.app.Activity;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -15,15 +21,20 @@ import java.util.Random;
 import android.view.Display;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
     int count = 0;
-    int timer = 10;
+    int timer = 2;
+    float sensorX, sensorY, sensorZ;
+    int frame = 0;
     ProgressBar bar = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
         bar = (ProgressBar)findViewById(R.id.time);
         bar.setMax(timer);
 
@@ -33,23 +44,7 @@ public class MainActivity extends AppCompatActivity {
         {
             public void onClick(View v)
             {
-                PopupWindow popupWin;
 
-                /*ポップアップに表示するレイアウトの設定*/
-                LinearLayout popLayout
-                        = (LinearLayout)getLayoutInflater().inflate(
-                        R.layout.popuptest, null);
-//                TextView popupText
-//                        = (TextView)popLayout.findViewById(R.id.textView);
-//                popupText.setText("Popup Text");
-
-                /*ポップアップの作成*/
-                popupWin = new PopupWindow(v);
-                popupWin.setWindowLayoutMode(
-                        WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
-                popupWin.setContentView(popLayout);
-                popupWin.setBackgroundDrawable(null);
-                popupWin.showAsDropDown(v, 0, 0);
                 //ボタンの下にポップアップを表示
             }
         });
@@ -58,77 +53,168 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    protected void onResume() {
+        super.onResume();
+        // Listenerの登録
+        Sensor accel = sensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    // 解除するコードも入れる!
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Listenerを解除
+        sensorManager.unregisterListener(this);
+    }
+    public void onSensorChanged(SensorEvent event) {
 
 
-        CountDownTimer countDownTimer = new CountDownTimer(10000000, 1000) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            sensorX = event.values[0];
+            sensorY = event.values[1];
+            sensorZ = event.values[2];
+            if(sensorX>10){
+                frame = 5;
+            }
+        }
+            String strTmp = "加速度センサー\n"
+                + " X: " + sensorX + "\n"
+                + " Y: " + sensorY + "\n"
+                + " Z: " + sensorZ;
+
+        Log.d("xyz", strTmp);
+
+    }
+    public void onAccuracyChanged(Sensor e,int i) {
+
+    }
+
+        CountDownTimer countDownTimer = new CountDownTimer(2000, 1000) {
             @Override
             public void onTick(long millsUntilFinished) {
-                onTv();
+
+                drawMogura();
+
+                if(frame > 0){
+                    frame--;
+                }
             }
 
             @Override
             public void onFinish() {
-                ;//finish();
+                PopupWindow popupWin;
+
+                /*ポップアップに表示するレイアウトの設定*/
+                LinearLayout popLayout
+                        = (LinearLayout)getLayoutInflater().inflate(
+                        R.layout.resultpopup, null);
+                TextView popupText
+                        = (TextView)popLayout.findViewById(R.id.scoreView);
+                popupText.setText(new Integer(count).toString());
+
+                /*ポップアップの作成*/
+                View v = getWindow().getDecorView();
+                popupWin = new PopupWindow(v);
+                popupWin.setWindowLayoutMode(
+                        WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+                popupWin.setContentView(popLayout);
+                popupWin.setBackgroundDrawable(null);
+                popupWin.showAsDropDown(v, 0, 0);
             }
         }.start();
 
 
 
-
-
-    public void onTv(View v) {
+    public void onTap(View v) {
         // カウントの加算
         count++;
-        //((TextView) findViewById(R.id.tv)).setText("" + count);
-        this.findViewById(R.id.moguraView).setVisibility(View.VISIBLE);
-        this.findViewById(R.id.moguraView2).setVisibility(View.VISIBLE);
-
-
-        // 表示位置の変更
-        int w = findViewById(R.id.activity_main).getWidth();
-        int w1 = (int) (w * 0.8);
-        int w2 = new Random().nextInt(w1);
-        //((TextView) findViewById(R.id.tv)).setTranslationX(w2);
-        this.findViewById(R.id.moguraView).setTranslationX(w2);
-        this.findViewById(R.id.moguraView2).setTranslationX(w2);
-
-
-        int h = findViewById(R.id.activity_main).getHeight();
-        int h1 = (int) (h * 0.8);
-        int h2 = new Random().nextInt(h1);
-        //((TextView) findViewById(R.id.tv)).setTranslationY(h2);
-        this.findViewById(R.id.moguraView).setTranslationY(h2);
-        this.findViewById(R.id.moguraView2).setTranslationY(h2);
+        drawMogura();
     }
-    public void onTv() {
+    public void drawMogura() {
         // カウントの加算
-        count++;
-        timer --;
+
+        timer--;
         bar.setProgress(timer);
+
+
         //((TextView) this.findViewById(R.id.tv)).setText("" + count);
         this.findViewById(R.id.moguraView).setVisibility(View.VISIBLE);
         this.findViewById(R.id.moguraView2).setVisibility(View.VISIBLE);
+        this.findViewById(R.id.moguraView3).setVisibility(View.GONE);
 
-//        if(count%2 == 0) {
+        int moguH = findViewById(R.id.moguraView).getHeight();
+        int mogu2H = findViewById(R.id.moguraView2).getHeight();
+        int moguW = findViewById(R.id.moguraView).getWidth();
+        int mogu2W = findViewById(R.id.moguraView2).getWidth();
+
+//        if (count % 2 == 0) {
 //            this.findViewById(R.id.moguraView).setVisibility(View.GONE);
 //            this.findViewById(R.id.moguraView2).setVisibility(View.GONE);//見えなくなるやつ
 //        }
-        // 表示位置の変更
+//         表示位置の変更
         WindowManager wm = getWindowManager();
         Display disp = wm.getDefaultDisplay();
-        int w = disp.getWidth();//this.findViewById(R.id.activity_main).getWidth();
-        int w1 = (int) (w * 0.8);
+        int displayW = disp.getWidth();//this.findViewById(R.id.activity_main).getWidth();
+
+
+        int w1 = (int) (displayW * 0.5);
         int w2 = new Random().nextInt(w1);
-        //((TextView) findViewById(R.id.tv)).setTranslationX(w2);
-        this.findViewById(R.id.moguraView).setTranslationX(w2);
-        this.findViewById(R.id.moguraView2).setTranslationX(w2);
-        int h = disp.getWidth();//(R.id.activity_main).getHeight();
-        int h1 = (int) (h * 0.8);
-        int h2 = new Random().nextInt(h1);
-        //((TextView) findViewById(R.id.tv)).setTranslationY(h2);
-        //((TextView) findViewById(R.id.tv)).setTextSize((int)(h2*0.1));
-        this.findViewById(R.id.moguraView).setTranslationX(h2);
-        this.findViewById(R.id.moguraView2).setTranslationY(h2);
-    }
+        if (w2 + mogu2W > displayW) {
+            w2 -= moguW;
+        }
+
+            //((TextView) findViewById(R.id.tv)).setTranslationX(w2);
+            this.findViewById(R.id.moguraView).setX(w2);
+
+
+            w2 = new Random().nextInt(w1);
+            if (w2 + mogu2W > displayW) {
+                w2 -= mogu2W;
+            }
+
+            this.findViewById(R.id.moguraView2).setX(w2);
+
+            w2 = new Random().nextInt(w1);
+            if (w2 + mogu2W > displayW) {
+                w2 -= mogu2W;
+            }
+
+            this.findViewById(R.id.moguraView3).setX(w2);
+
+            int displayH = disp.getWidth();//(R.id.activity_main).getHeight();
+
+            int h1 = (int) (displayH * 0.5);
+            int h2 = new Random().nextInt(h1);
+            if (h2 + moguH > displayH) {
+                h2 -= moguH;
+            }
+            //((TextView) findViewById(R.id.tv)).setTextSize((int)(h2*0.1));
+            this.findViewById(R.id.moguraView).setTranslationY(h2);
+
+
+            h2 = new Random().nextInt(h1);
+            if (h2 + mogu2H > displayH) {
+                h2 -= mogu2H;
+
+            }
+
+            this.findViewById(R.id.moguraView2).setTranslationY(h2);
+
+        h2 = new Random().nextInt(h1);
+        if (h2 + mogu2H > displayH) {
+            h2 -= mogu2H;
+
+        }
+
+        this.findViewById(R.id.moguraView3).setTranslationY(h2);
+
+        if(frame>=1){
+            this.findViewById(R.id.moguraView3).setVisibility(View.VISIBLE);
+        }
+        }
 
 }
